89e25cad5af6c3295c09d1b86dda9c6f91f4b780
mdmudassir7 <mohammedmudassir200@gmail.com>
2022-03-13 16:51:42 +0530

    Add linkedListCycleII

---
+++ linkedListCycleII/README.md
@@ -0,0 +1,12 @@
+Approach
+
+    1.Using the 2 pointer approach we find if there exists a cycle in our list
+
+    2.Initialise 2 pointers slow and fast, slow moves one step at a time and fast moves 2 steps
+
+    3.It is guarenteed that the distance of point of intersection of slow and fast pointers from the start of cycle is same 
+
+    as its distance from the head
+
+
+![CycleII](sample.jpg)
---
+++ linkedListCycleII/code.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+ };
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while(fast && fast->next){
+            slow = slow->next;
+            fast = fast->next->next;
+            if(slow==fast)
+                break;
+        }
+        if(!(fast and fast->next))
+            return NULL;
+        while(head!=slow)
+        {
+            head=head->next;
+            slow=slow->next;
+        }
+        return head;
+    }
+};
